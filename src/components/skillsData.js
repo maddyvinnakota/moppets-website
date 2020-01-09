8991246e3d21c6f57587859d360e1c143f407197
Kerstin Dengl <kerstin.dengl@gmail.com>
2020-01-09 18:17:55 +0100

    longer tech stack

--- src/components/skillsData.js
+++ src/components/skillsData.js
@@ -1,52 +1,55 @@
 let counter = 1
 
-export const tech = [
-  {
-    id: `tech-${counter}`,
-    name: 'React',
-    skillLevel: 3,
-  },
-  // {
-  //   id: `tech-${counter+=1}`,
-  //   name: "Gatsby",
-  //   skillLevel: 3,
-  // },
-  {
-    id: 'tech-3',
-    name: 'JS',
-    skillLevel: 3,
-  },
-  {
-    id: 'tech-4',
-    name: 'SASS',
-    skillLevel: 4,
-  },
-  {
-    id: 'tech-5',
-    name: 'CSS',
-    skillLevel: 5,
-  },
-  {
-    id: 'tech-6',
-    name: 'Flexbox',
-    skillLevel: 5,
-  },
-  {
-    id: 'tech-7',
-    name: 'HTML',
-    skillLevel: 5,
-  },
-  {
-    id: 'tech-8',
-    name: 'Drupal',
-    skillLevel: 2,
-  },
-  {
-    id: 'tech-9',
-    name: 'Git',
-    skillLevel: 3,
-  },
-]
+export const tech = ["React", "JavaScript", "Drupal", "HTML5", 'CSS3', "SASS", "Node.js",
+  "Express", "MongoDB/Mongoose", "Ava", "Docker", "Heroku", "ESLint", "Husky", "Prettier"]
+  
+  // [
+//   {
+//     id: `tech-${counter}`,
+//     name: 'React',
+//     skillLevel: 3,
+//   },
+//   // {
+//   //   id: `tech-${counter+=1}`,
+//   //   name: "Gatsby",
+//   //   skillLevel: 3,
+//   // },
+//   {
+//     id: 'tech-3',
+//     name: 'JS',
+//     skillLevel: 3,
+//   },
+//   {
+//     id: 'tech-4',
+//     name: 'SASS',
+//     skillLevel: 4,
+//   },
+//   {
+//     id: 'tech-5',
+//     name: 'CSS',
+//     skillLevel: 5,
+//   },
+//   {
+//     id: 'tech-6',
+//     name: 'Flexbox',
+//     skillLevel: 5,
+//   },
+//   {
+//     id: 'tech-7',
+//     name: 'HTML',
+//     skillLevel: 5,
+//   },
+//   {
+//     id: 'tech-8',
+//     name: 'Drupal',
+//     skillLevel: 2,
+//   },
+//   {
+//     id: 'tech-9',
+//     name: 'Git',
+//     skillLevel: 3,
+//   },
+// ]
 
 export const designTools = [
   {
